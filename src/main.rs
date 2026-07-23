bab0e7d1cd34bbfdfdf2abad3bb67caebb9c89d0
dbsc <Danilbds21@yandex.ru>
2026-07-23 23:32:47 +0700

    Implement local file reader

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 fn main() {
     let path = "/home/dbsc/disk/text.txt";
-    let contents = fs::read_to_string(path).expect("can't read files");
-    println!("{}", contents);
+    let contents = fs::read(path).expect("can't read files");
+    println!("{:?}", contents);
 }
