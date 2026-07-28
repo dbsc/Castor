275ec84c8aba7f52c574a846a87e71a5628ffa4c
dbsc <Danilbds21@yandex.ru>
2026-07-28 21:21:45 +0700

    Alpha Shredder added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use std::fs;
-fn main() {
-    let path = "/home/dbsc/disk/text.txt";
+fn reader() -> Vec<u8> {
+    let path = "/home/dbsc/litelab-crm/docker-compose.yml";
     let contents = fs::read(path).expect("can't read files");
-    println!("{:?}", contents);
+    contents
+}
+fn crasher() -> String {
+    let check = reader();
+    let mut full = String::new();
+    let kol = check.len();
+    for i in 0..kol {
+        let num_toas = check[i].to_string();
+        full.push_str(&num_toas);
+    }
+    full
+}
+fn main() {
+    let rusult = crasher();
+    println!("{rusult:?}");
 }
